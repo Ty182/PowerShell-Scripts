853c27b5d54b08688532db7c2903d9db701c5cb8
Ty182 <54518668+Ty182@users.noreply.github.com>
2020-05-09 13:45:46 -0500

    Add files via upload

---
+++ findServicePathQuotes.ps1
@@ -0,0 +1,22 @@
+﻿# Author: Tyler Petty
+# Date: 5/9/2020
+# Description: Find either quoted or unquoted service paths
+
+# Do a service dump and save all service names to array, $services
+$services = @(Get-Service | Select-Object -Property Name)
+
+foreach ($service in $services) 
+    { 
+        # Execute a search on services and return only the BINARY_PATH_NAME from the output
+        # \s Matchs the whitespace
+        # * Match any number of times
+        # : The output contained a :
+        # \s Match the one whitespace
+        # "C: Match the text
+
+        # return all quoted service paths
+        sc.exe qc $service.Name | Select-String -Pattern 'BINARY_PATH_NAME\s*:\s"C:'
+
+        # return all unquoted service paths
+        sc.exe qc $service.Name | Select-String -Pattern 'BINARY_PATH_NAME\s*:\sC:'
+    }
